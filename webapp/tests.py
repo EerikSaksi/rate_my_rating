7d6c092f154c46d2811127ea8aef11e7bae171a4
Eerik Saksi <2392230s@student.gla.ac.uk>
2019-03-20 21:20:46 +0000

    Started creating tests

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from webapp import models
+from django.urls import resolve, Resolver404
 
-# Create your tests here.
+
+def each_website_has_correctly_mapped_url():
+    ratingWebsiteSlugs=RatingWebsite.objects.all().values_list('slug', flat=True)
+
+    for slug in ratingWebsiteSlugs:
+        try:
+            resolve("websites/"+slug)
+        except Resolver404:
+            print("Website with %s slug doesn't map"%(slug))
+        else:
+            print("Website with %s slug is mapped" %(slug))
+
+
+def newWebsitesAreOrganized():
+    
+
+def test():
+    each_website_has_correctly_mapped_url()
+
+
+
+
+test()
+
+
+
+
+
