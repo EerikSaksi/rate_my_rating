17c8284b6ad38225d2f30db00ddc712e5c1392fd
David Jasek <2398089J@student.gla.ac.uk>
2019-03-21 12:39:24 +0100

    moved js and css to separate files

---
+++ static/js/about.js
@@ -0,0 +1,8 @@
+function onClick() {
+    var x = document.getElementById("displayOnClick");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
